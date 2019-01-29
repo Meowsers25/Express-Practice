db4db58142bb0d705a1604e811ca87e8818fa1e7
Chris <c.lamothe25@gmail.com>
2019-01-29 15:22:18 -0500

    Initial commit for Express practice

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log('Our first Express app')
+const express = require('express')
+const app = express()
+
+app.get('/', function(req, res){
+    res.send('Hi there!')
+})
+
+app.get('/bye', function(req, res){
+    res.send('Goodbye')
+})
+
+app.get('/dog', function(req, res){
+    console.log('SOMEONE MADE A REQUEST TO DOG....')
+    res.send('MEOW!')
+})
+
+app.listen(3000, function(){
+    console.log('Server has started....')
+})
