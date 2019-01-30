d9672393c8e70b295e0bc173e5b52e3f68ff385e
Chris <c.lamothe25@gmail.com>
2019-01-30 15:43:27 -0500

    Realized splat needs to come last after other routes

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ console.log('Our first Express app')
 const express = require('express')
 const app = express()
 
-app.get('*', function(req, res){
-    res.send('YOURE A STAR')
-})
-
 app.get('/', function(req, res){
     res.send('Hi there!')
 })
@@ -17,6 +13,10 @@ app.get('/bye', function(req, res){
 app.get('/dog', function(req, res){
     console.log('SOMEONE MADE A REQUEST TO DOG....')
     res.send('MEOW!')
+})
+
+app.get('*', function(req, res){
+    res.send('YOURE A STAR')
 })
 
 app.listen(3000, function(){
